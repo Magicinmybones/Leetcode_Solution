99953f4acd209504e22bc0aa86b22c231f3afd95
Magicinmybones <150594049+Magicinmybones@users.noreply.github.com>
2025-03-17 23:55:25 +0530

    Added 387

---
+++ 387-First-Unique-Character-In-A-String/387-First-Unique-Character-In-A-String.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+int firstUniqChar(string s);
+
+int main() {
+    string s = "leetcode";
+    int index = firstUniqChar(s);
+    if (index != -1) {
+        cout << "The first unique character is at index: " << index << endl;
+    } else {
+        cout << "There is no unique character in the string." << endl;
+    }
+    return 0;
+}
+int firstUniqChar(string s) {
+    int n = s.size();
+    unordered_map<char,int> hash;
+    queue <int> q;
+    for(int i = 0;i<n;i++){
+        if(hash[s[i]]==0){
+            q.push(i);
+        }
+        if(hash[s[i]]<=1){
+        hash[s[i]]++;
+        }
+    }
+    while(!q.empty()){
+        if(hash[s[q.front()]]==1){
+            return q.front();
+        }
+        q.pop();
+    }
+    return -1;
+}
